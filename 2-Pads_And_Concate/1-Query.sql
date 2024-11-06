8163109d272cd27be65c52d4501add22b3cfe2fa
Sadam Barkat <157804568+Sadam-Barkat@users.noreply.github.com>
2024-11-07 01:31:31 +0500

    Add files via upload

---
+++ 2-Pads_And_Concate/1-Query.sql
@@ -0,0 +1,27 @@
+/*
+Problem:
+1-Query an alphabetically ordered list of all names in OCCUPATIONS,
+ immediately followed by the first letter of each profession as a
+ parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), 
+ ADoctorName(D), AProfessorName(P), and ASingerName(S).
+
+2-Query the number of ocurrences of each occupation in OCCUPATIONS.
+  Sort the occurrences in ascending order, and output them in the following format:
+
+Link:
+https://www.hackerrank.com/challenges/the-pads/problem?isFullScreen=true
+*/
+
+#Solution:
+
+SELECT 
+    CONCAT(name, '(', LEFT(occupation, 1), ')') AS name_with_profession
+FROM OCCUPATIONS
+ORDER BY name;
+
+
+SELECT 
+    CONCAT('There are a total of ', COUNT(occupation), ' ', LOWER(occupation), 's.') AS occupation_count
+FROM OCCUPATIONS
+GROUP BY occupation
+ORDER BY COUNT(occupation) ASC, occupation;
