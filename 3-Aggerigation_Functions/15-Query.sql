b84ea17f7cf3f35267c47a0587561053a8701152
Sadam Barkat <157804568+Sadam-Barkat@users.noreply.github.com>
2025-01-19 22:41:10 +0500

    Add files via upload

---
+++ 3-Aggerigation_Functions/15-Query.sql
@@ -0,0 +1,21 @@
+/*
+Problem:
+Query the Manhattan Distance between points  and  and round it to a scale of 4 decimal places.
+
+Problem Link:
+https://www.hackerrank.com/challenges/weather-observation-station-18/problem?isFullScreen=true
+*/
+
+#Problem Solution
+
+WITH cte AS
+(SELECT
+      MIN(LAT_N) AS a,
+      MIN(LONG_W) AS b,
+      MAX(LAT_N) AS c,
+      MAX(LONG_W) AS d
+      FROM Station
+)    
+SELECT 
+      ROUND((c - a) + (d - b),4)
+      FROM cte;
